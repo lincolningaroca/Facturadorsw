0b8631370eb264a7e4a88f0468eca53a48beb0bd
Lincoln Ingaroca <lincolningaroca@gmail.com>
2025-09-20 20:43:26 -0500

    .gitignore file update.

--- build/Desktop_Qt_6_8_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_facturadorsw_FILE D:/QtProjects/facturadorsw/build/Desktop_Qt_6_8_3_MinGW_64_bit-Debug/facturadorsw.exe)
-set(__QT_DEPLOY_TARGET_facturadorsw_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_facturadorsw_RUNTIME_DLLS C:/Qt/6.8.3/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.3/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.3/mingw_64/bin/Qt6Core.dll)
